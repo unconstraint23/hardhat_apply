95744864f23454383864a33bc189eeaddfe0856f
zhangrunjing <527667654@qq.com>
2025-08-15 00:08:03 +0800

    hardhat学习

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades')
+require("hardhat-deploy")
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  namedAccounts: {
+     deployer: 0,
+    user1: 1,
+    user2: 2,
+
+  }
 };
---
+++ test/nftAction.js
@@ -0,0 +1,24 @@
+const { ethers } = require('hardhat')
+
+describe('Starting NftActions', () => {
+    it('should deploy', async () => {
+        const nftAction = await ethers.getContractFactory('NftAction')
+        const nftActionContract = await nftAction.deploy()
+        await nftActionContract.waitForDeployment()
+        console.log('nftActionContract deployed to:', nftActionContract.address)
+        await nftActionContract.createNftAction(
+            100 * 1000,
+            ethers.parseEther('0.00000001'),
+            ethers.ZeroAddress,
+            1,
+        )
+        const nftActionId = await nftActionContract.nextNftActionId()
+        console.log('nftActionId:', nftActionId)
+        const nftActionInfo = await nftActionContract.nftActions(nftActionId)
+        console.log('nftActionInfo:', nftActionInfo)
+
+        const action = await nftActionContract.Action(0)
+        console.log('action:', action)
+
+    })
+})
