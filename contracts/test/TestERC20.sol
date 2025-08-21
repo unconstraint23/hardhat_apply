1d2d41757c2825a9eb5e1aae2f5bcd94a32a7dab
zhangrunjing <527667654@qq.com>
2025-08-21 17:30:23 +0800

    代码修改

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,52 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract TestERC20 is IERC20 {
- 
-    mapping(address account => uint256) private _balances;
+pragma solidity ^0.8.20;
 
-    mapping(address account => mapping(address spender => uint256)) private _allowances;
-     address immutable owner;
-    constructor() {
-        owner = msg.sender;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TestERC20 is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        _mint(msg.sender, 100000 * 10 ** 18);
     }
-     modifier onlyOwner {
-        require(msg.sender == owner, "only owner can call");
-        _;
-    }
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-    function transfer(address to, uint256 amount) public returns (bool) {
-        require(amount > 0 ,"amount must be more than 0");
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-        _balances[msg.sender] -= amount;
-        _balances[to] += amount;
-        emit Transfer(msg.sender, to, amount);
-        return true;
-    }
-    function mint(uint256 amount) external onlyOwner {
-        _balances[msg.sender] += amount;
-    }
-    function totalSupply() external view returns (uint256) {
-        return _balances[msg.sender];
-    }
-    function allowance(address _owner, address spender) external view returns (uint256) {
-        return _allowances[_owner][spender];
-    }
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        require(amount > 0, "amount must be more than 0");
-        require(_allowances[msg.sender][from] >= amount, "exceeds approved amount");
-        require(_balances[from] >= amount, "Insufficient balance");
-        _allowances[msg.sender][from] -= amount;
-        _balances[from] -= amount;
-        _balances[to] += amount;
-        emit Transfer(from, to, amount);
-        return true;    
-    }    
 }
